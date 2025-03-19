fe1a2cfb06971ba93414c187bdc8473c1e637343
eslams3dawi <eslamseadawy717@gmail.com>
2025-03-19 05:59:51 +0200

    Add API Layer

---
+++ DEPI.API/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using DEPI.BLL.DTOs.StudentDTO;
+using DEPI.BLL.Services.StudentService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+
+        public readonly IStudentService _studentService;
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+        [HttpGet]
+        public ActionResult GetAll()
+        {
+            return Ok(_studentService.GetAll());
+        }
+        [HttpGet("{id}")]
+        public ActionResult GetById(int id)
+        {
+            return Ok(_studentService.GetById(id));
+        }
+        [HttpPost]
+        public ActionResult Add(StudentAddDTO student)
+        {
+            _studentService.Add(student);
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, StudentUpdateDTO student)
+        {
+            if(id != student.Id)
+                return BadRequest();
+
+            _studentService.Update(id, student);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            _studentService.Delete(id);
+            return NoContent();
+        }
+
+    }
+}
